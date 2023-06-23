85bd032ff72349f3569e01852941fb99de8d4fb7
PHLep <137480629+PHLep@users.noreply.github.com>
2023-06-23 16:26:13 +0800

    Add files via upload

---
+++ JS/Login2.js
@@ -0,0 +1,18 @@
+﻿function Sample()
+
+{
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    if (username == "admin" && password == "password")
+    {
+        alert("Login Successful");
+        return true;
+    }
+    else
+    {
+        alert("Invalid Username and Password");
+        return true;
+    }
+  
+}
